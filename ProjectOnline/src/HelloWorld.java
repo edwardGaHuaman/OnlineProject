9c62fd1e1c2689fd590e6525a7f509334442ffb7
edwardGaHuaman <huamanmeneses.edward@hotmail.fr>
2014-12-18 15:38:54 +0100

    test commit push 2 Signed-off-by: edwardGaHuaman <huamanmeneses.edward@hotmail.fr>

--- ProjectOnline/src/HelloWorld.java
+++ ProjectOnline/src/HelloWorld.java
@@ -3,11 +3,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		int a = 6;
-		int b = 5;
-		String z = "azeaz";
+		int b = 14;
+		
 		int c = a+b;
 		System.out.println("Hello World "+c);
-		System.out.println("Chingados");
+		System.out.println("Salut, bonjour");
 	}
 
 }
