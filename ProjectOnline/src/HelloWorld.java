fa0905021a04fdc7c859d29438fc9286b796f8f9
edwardGaHuaman <huamanmeneses.edward@hotmail.fr>
2014-12-18 15:26:59 +0100

    * test

---
+++ ProjectOnline/src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 6;
+		int b = 3;
+		
+		int c = a+b;
+		System.out.println("Hello World "+c);
+	}
+
+}
