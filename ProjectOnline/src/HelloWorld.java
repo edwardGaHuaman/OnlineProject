53c6a5f2b77980bcdc5d267d3a029db796228bd3
edwardGaHuaman <huamanmeneses.edward@hotmail.fr>
2014-12-18 15:37:36 +0100

    * testetststet

--- ProjectOnline/src/HelloWorld.java
+++ ProjectOnline/src/HelloWorld.java
@@ -1,11 +1,10 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		int a = 6;
 		int b = 5;
-		
+		String z = "azeaz";
 		int c = a+b;
 		System.out.println("Hello World "+c);
 		System.out.println("Chingados");
